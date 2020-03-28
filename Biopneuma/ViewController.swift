eb2735df01872c98cfffb4f05f2043613f7276d5
Ishan Jain <ishanjain42@gmail.com>
2020-03-28 16:50:54 -0700

    commit

--- Biopneuma/ViewController.swift
+++ Biopneuma/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 10
     }
 
 
